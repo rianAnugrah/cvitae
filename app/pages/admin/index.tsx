4cbba36224619fa9669b9ba66c1ddb117b3f334b
Rian Anugrah <rian.te.quiero@gmail.com>
2023-12-24 18:05:57 +0700

    chnge

--- app/pages/admin/index.tsx
+++ app/pages/admin/index.tsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { NextSeo } from "next-seo";
 import React, { useEffect, useState } from "react";
 
-import { Viewport, RenderNode } from "../../components/editor";
-import { Container, Text } from "../../components/selectors";
-import { Button } from "../../components/selectors/Button";
-import { Custom1, OnlyButtons } from "../../components/selectors/Custom1";
-import { Custom2, Custom2VideoDrop } from "../../components/selectors/Custom2";
-import { Custom3, Custom3BtnDrop } from "../../components/selectors/Custom3";
-import { Video } from "../../components/selectors/Video";
+import { Viewport, RenderNode } from "../../../components/editor";
+import { Container, Text } from "../../../components/selectors";
+import { Button } from "../../../components/selectors/Button";
+import { Custom1, OnlyButtons } from "../../../components/selectors/Custom1";
+import { Custom2, Custom2VideoDrop } from "../../../components/selectors/Custom2";
+import { Custom3, Custom3BtnDrop } from "../../../components/selectors/Custom3";
+import { Video } from "../../../components/selectors/Video";
 import axios from "axios";
 
 const theme = createMuiTheme({
