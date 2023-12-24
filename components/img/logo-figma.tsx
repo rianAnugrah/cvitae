b0d459c2dbc181059e76cd14cb6d1b854e54068d
Rian Anugrah <rian.te.quiero@gmail.com>
2023-12-24 17:31:01 +0700

    Update logo-figma.tsx

--- components/img/logo-figma.tsx
+++ components/img/logo-figma.tsx
@@ -13,31 +13,31 @@ export default function LogoFigma() {
       <desc>Created using Figma</desc>
       <path
         id="path0_fill"
-        class="st0"
+        className="st0"
         fill="#0acf83"
         d="M50 300c27.6 0 50-22.4 50-50v-50H50c-27.6 0-50 22.4-50 50s22.4 50 50 50z"
       />
       <path
         id="path1_fill"
-        class="st1"
+        className="st1"
         fill="#a259ff"
         d="M0 150c0-27.6 22.4-50 50-50h50v100H50c-27.6 0-50-22.4-50-50z"
       />
       <path
         id="path1_fill_1_"
-        class="st2"
+        className="st2"
         fill="#f24e1e"
         d="M0 50C0 22.4 22.4 0 50 0h50v100H50C22.4 100 0 77.6 0 50z"
       />
       <path
         id="path2_fill"
-        class="st3"
+        className="st3"
         fill="#ff7262"
         d="M100 0h50c27.6 0 50 22.4 50 50s-22.4 50-50 50h-50V0z"
       />
       <path
         id="path3_fill"
-        class="st4"
+        className="st4"
         fill="#1abcfe"
         d="M200 150c0 27.6-22.4 50-50 50s-50-22.4-50-50 22.4-50 50-50 50 22.4 50 50z"
       />
